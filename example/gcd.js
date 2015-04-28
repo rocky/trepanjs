bab9549b0a6c13ad3ca17ea9cd55d0c11e21fe4a
rocky <rockyb@rubyforge.org>
2015-04-27 22:40:42 -0400

    Add 'up', 'down', 'frame', and frame handling setBreakpoint: fix bug in showing line number gcd.js: simplify

--- example/gcd.js
+++ example/gcd.js
@@ -2,24 +2,6 @@
 var util = require("util");
 require("console");
 
-function ask(question, format, callback) {
- var stdin = process.stdin, stdout = process.stdout;
-
- stdin.resume();
- stdout.write(question + ": ");
-
- stdin.once('data', function(data) {
-   data = data.toString().trim();
-
-   if (format.test(data)) {
-     callback(data);
-   } else {
-     stdout.write("It should match: "+ format +"\n");
-     ask(question, format, callback);
-   }
- });
-}
-
 // GCD. We assume positive numbers
 function gcd(a, b) {
     // Make: a <= b
