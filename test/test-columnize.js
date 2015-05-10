9b52ec96bd8734a93e268d1b7543607cb14af713
rocky <rockyb@rubyforge.org>
2015-05-10 02:52:01 -0400

    use strict more pervasively one more columnize test split out sourceInfo function

--- test/test-columnize.js
+++ test/test-columnize.js
@@ -13,4 +13,10 @@ describe('columnize', function() {
     it('columnize one line', function() {
 	col(['one', 'two', 'three'], opts).should.equal('one  two  three\n');
     });
+
+    it('columnize two lines', function() {
+	opts = {displayWidth: 4, colsep: '  '};
+	col(['1', '2', '3', '4'], opts).should.equal("1  3\n2  4\n");
+    });
+
 });
